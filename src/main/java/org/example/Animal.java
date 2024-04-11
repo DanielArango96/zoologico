8a7d46c1612c159518803473901848a4c0750fdc
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2024-04-11 10:02:39 -0500

    Se guardan corecciones

--- src/main/java/org/example/Animal.java
+++ src/main/java/org/example/Animal.java
@@ -2,13 +2,47 @@ package org.example;
 
 public class Animal {
 
-    String nombre;
-    String genero
-    double peso=true;
-    Animal pareja=new;
+   private String nombre;
+   private String genero;
+   private  double peso;
+    public Animal pareja;
 
+    public Animal() {
+    }
 
-    public Animal(String nombre, double peso) {
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public Animal getPareja() {
+        return pareja;
+    }
+
+    public void setPareja(Animal pareja) {
+        this.pareja = pareja;
+    }
+
+    public Animal(String nombre) {
         this.nombre = nombre;
         this.peso = peso;
     }
--- src/main/java/org/example/Familia.java
+++ src/main/java/org/example/Familia.java
@@ -1,50 +1,50 @@
 package org.example;
 
-public class Familia {
 
-    Animal papa="Dinosaurin";
-    Animal mama="Tiburosin";
-    Animal hijo;
+    public class Familia {
 
-    public Familia(Animal papa, Animal mama) {
-        this.papa = papa;
-        this.mama = mama;
-    }
+        Animal papa = new Animal("Dinosaurin");
+        Animal mama = new Animal("Tiburosin");
+        Animal hijo= new Animal();
 
-    public  imprimirFamilia(){
+        public Familia(Animal papa, Animal mama) {
+            this.papa = papa;
+            this.mama = mama;
+        }
 
-        //Este metodo muestra los nombres de los integrantes de la familia
-        system.out.println(
-                "El papá es: "+this.papa.nombre+
-                        "\nLa mamá es: "+this.mama.nombre+
-                            "\nEl hijo es: "+this.hijo.nombre
-        )
+        public void imprimirFamilia() {
 
-    }
+            //Este metodo muestra los nombres de los integrantes de la familia
+            System.out.println(
+                    "El papá es: " + this.papa.getNombre() +
+                            "\nLa mamá es: " + this.mama.getNombre() +
+                            "\nEl hijo es: " + this.hijo.getNombre()
+            );
 
-    public void tenerHijo(String nombre){
+        }
 
-        if(this.papa.pareja!=null){
+        public void tenerHijo(String nombre) {
 
-            Animal hijo= new Animal();
-            hijo.nombre=nombre;
-            hijo.peso=1;
-            double random = Math.random();
-            if(random<0.5){
-                hijo.genero="F";
-            }else{
-                hijo.genero="M";
+            if (this.papa != null && this.papa != null) {
+
+                Animal hijo = new Animal();
+                hijo.setNombre(nombre);
+                hijo.setPeso(1);
+                double random = Math.random();
+                if (random < 0.5) {
+                    hijo.setGenero("F");
+                } else {
+                    hijo.setGenero("M");
+                }
+                this.hijo = hijo;
+
+            } else {
+                System.out.println("Aun no puedes tener hijos");
             }
-            this.hijo=hijo;
 
-        }else{
-            System.out.println("Aun no puedes tener hijos");
+
         }
 
 
     }
 
-
-
-
-}
